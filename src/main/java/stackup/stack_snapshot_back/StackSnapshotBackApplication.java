bbbb8a725b721fe0e5d04a86f3244b64a0e2cae3
minji <alswlchlrh8@naver.com>
2024-10-10 23:10:56 +0900

    Initial commit

---
+++ src/main/java/stackup/stack_snapshot_back/StackSnapshotBackApplication.java
@@ -0,0 +1,13 @@
+package stackup.stack_snapshot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackSnapshotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackSnapshotBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stackup/stack_snapshot_back/StackSnapshotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package stackup.stack_snapshot_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackSnapshotBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

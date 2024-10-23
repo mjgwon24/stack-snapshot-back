2dbf70108cea306534ddfe16a199cd10f72d5b44
minji <alswlchlrh8@naver.com>
2024-10-23 23:29:44 +0900

    [UPDATE] build 파일 추가 및 환경변수 개선

--- src/main/java/stackup/stack_snapshot_back/config/WebConfig.java
+++ src/main/java/stackup/stack_snapshot_back/config/WebConfig.java
@@ -1,5 +1,6 @@
 package stackup.stack_snapshot_back.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,10 +12,13 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @Value("${server.cross-origin-url")
+            private String crossOriginUrl;
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(crossOriginUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);

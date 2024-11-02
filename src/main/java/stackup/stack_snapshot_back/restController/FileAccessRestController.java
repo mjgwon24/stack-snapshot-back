b82973e352458bbd2abb2aeb67db64dfb6b54761
kimehyun <lh7721004@naver.com>
2024-11-03 00:01:54 +0900

    [ADD] 파일 접근 API 추가

---
+++ src/main/java/stackup/stack_snapshot_back/restController/FileAccessRestController.java
@@ -0,0 +1,63 @@
+package stackup.stack_snapshot_back.restController;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stackup.stack_snapshot_back.service.FileAccessService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+@RestController
+@RequestMapping("/api")
+@PropertySource("classpath:application.yml") //application.yml에 들어있는 데이터 사용, Service에서는 사용 못하기 때문에 넘겨줘야함
+public class FileAccessRestController {
+    @Value("${file.upload-dir}")
+    String STATIC_DIR;
+    String UPLOAD_PATH, OUTPUT_PATH, FRAME_PATH;
+
+
+
+    // 프로그램이 실행되면 변수 초기화
+    @PostConstruct
+    public void initPaths() {
+        UPLOAD_PATH = STATIC_DIR + "/original-photo/";
+        OUTPUT_PATH = STATIC_DIR + "/final-photo/";
+        FRAME_PATH = STATIC_DIR + "/frames/";
+    }
+    @CrossOrigin("*")
+    @GetMapping("/file")
+    public ResponseEntity<Resource> getFile(@RequestParam String date, @RequestParam String groupid, @RequestParam String index) throws IOException {
+        // group_test_20241101_010321_0.jpg
+        try{
+            FileAccessService fileAccessService = new FileAccessService();
+            return fileAccessService.read_File(groupid,date,index,UPLOAD_PATH,false);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @CrossOrigin("*")
+    // 사용자가 접근 할 수 있어야 하는 경로이므로 Cross Origin 적용 x
+    @GetMapping("/final_file")
+    public ResponseEntity<Resource> getFinalFile(@RequestParam String date, @RequestParam String groupid) throws IOException {
+        // 파일 경로 설정 (프로젝트 외부의 파일 경로)
+        // group_test_20241101_010321_0.jpg
+        // group_test_final_20241101_224410.png
+        try{
+            FileAccessService fileAccessService = new FileAccessService();
+            return fileAccessService.read_File(groupid,date,null,OUTPUT_PATH,true);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+}

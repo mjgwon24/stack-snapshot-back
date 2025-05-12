54e1cddec17e856bbcb5f03d406002f3512ddc0b
kimehyun <lh7721004@gmail.com>
2025-05-13 01:55:29 +0900

    update: ServiceLayer 반환값 수정

--- src/main/java/stackup/stack_snapshot_back/restController/RewardRestController.java
+++ src/main/java/stackup/stack_snapshot_back/restController/RewardRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import stackup.stack_snapshot_back.dto.RewardResponseDto;
 import stackup.stack_snapshot_back.entity.RewardStatus;
 import stackup.stack_snapshot_back.service.RewardService;
 
@@ -18,7 +19,7 @@ import stackup.stack_snapshot_back.service.RewardService;
 public class RewardRestController {
     private final RewardService rewardService;
     @GetMapping
-    public ResponseEntity<RewardStatus> requestReward() {
+    public ResponseEntity<RewardResponseDto> requestReward() {
         return ResponseEntity.ok(rewardService.getReward());
     }
 }

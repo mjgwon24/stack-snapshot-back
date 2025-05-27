e5833eebd1c1f7c77649bb8024f50661f3b00f69
kimehyun <lh7721004@gmail.com>
2025-05-27 10:18:56 +0900

    update: 경품 뽑기 로직 개선 및 최종 이미지 생성 로직 개선

---
+++ src/main/java/stackup/stack_snapshot_back/entity/Reward.java
@@ -0,0 +1,53 @@
+package stackup.stack_snapshot_back.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Reward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reward_id",unique = true)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RewardStatus rewardType;
+
+    @Column(nullable = false)
+    private Integer maxCount;
+
+    @Column(nullable = false)
+    private Integer totalCount;
+
+    @Column(nullable = false)
+    private Integer remainingCount;
+
+    @DecimalMax("1.0")
+    @DecimalMin("0.0")
+    @Column(nullable = false)
+    private Double probability;
+
+    @Column(nullable = false)
+    private Boolean isAvailable;
+
+    public void choice(){
+        this.remainingCount -= 1;
+        this.totalCount += 1;
+        if(this.remainingCount == 0){
+            this.isAvailable = false;
+        }
+    }
+
+    public void setAvailable(Boolean available){
+        this.isAvailable = available;
+    }
+}
---
+++ src/main/java/stackup/stack_snapshot_back/entity/RewardStatus.java
@@ -0,0 +1,10 @@
+package stackup.stack_snapshot_back.entity;
+
+public enum RewardStatus {
+    USB,
+    GIFTICON50000,
+    GIFTICON30000,
+    GIFTICON10000,
+    CANDY,
+    FAIL
+}
